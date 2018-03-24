6679438f804739da760ee494e9e413fa5e5a7b6c
Adrian Miranda <miranda_adrian@hotmail.com>
2018-03-24 12:43:33 -0600

    Log

--- src/main/java/com/radarez/apirest/service/NotaService.java
+++ src/main/java/com/radarez/apirest/service/NotaService.java
@@ -4,6 +4,8 @@ import com.radarez.apirest.entity.Nota;
 import com.radarez.apirest.model.MNota;
 import com.radarez.apirest.repository.NotaRepositorio;
 import com.radarez.apirest.converter.Convertidor;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -21,29 +23,40 @@ public class NotaService {
     @Qualifier("convertidor") /*@Component("convertidor")*/
     public Convertidor convertidor;
 
+    private static final Log logger = LogFactory.getLog(NotaService.class);
+
     public boolean crear(Nota nota){
+        logger.info("Creando nota...");
         try{
             repositorio.save(nota);
+            logger.info("Se creó una nota =)");
             return true;
         }catch (Exception e){
+            logger.error("No se creó la nota :(");
             return false;
         }
     }
     public boolean actualizar(Nota nota){
+        logger.info("Actiualizando nota...");
         try{
-            //Validate that record is not null or 0
+            //TODO: Validate that record is not null or 0
             repositorio.save(nota);
+            logger.info("Se actualizo la nota: " + nota.getId());
             return true;
         }catch (Exception e){
+            logger.error("Ocurrio un erro al actualizar la nota");
             return false;
         }
     }
     public boolean borrar(String nombre, long id){
+        logger.info("Eliminado nota...");
         try{
             Nota nota = repositorio.findByNombreAndId(nombre, id);
             repositorio.delete(nota);
+            logger.info("Se elimino la nota de: " + nombre + " con Id:" + id);
             return true;
         }catch (Exception e){
+            logger.error("Ocurrio un error al eliminar la nota :(");
             return false;
         }
     }
